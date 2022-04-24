32ebf25712d3cbf323e5e61359c491c742c1aa79
Samartean Rares Petru <84010916+EightSeven60@users.noreply.github.com>
2022-04-24 18:33:20 +0300

    Implemented modern encryption algorithm; HAVEN'T TESTED IT YET

--- app/src/main/java/com/example/offlinepasswordmanager/Cryptography/CryptoHandler.java
+++ app/src/main/java/com/example/offlinepasswordmanager/Cryptography/CryptoHandler.java
@@ -10,11 +10,15 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.security.AlgorithmParameters;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
 import java.security.spec.AlgorithmParameterSpec;
 
 import javax.crypto.Cipher;
@@ -25,54 +29,89 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class CryptoHandler {
     private static final String TAG = "CRYPTOHANDLER";
+    private static final String keyAlias = "AESStringsEncryptionKey";
+    private static final String ivAlias = "AESStringsEncryptionIv";
+    private static final String keyStoreFileName = "keystorage";
     private static CryptoHandler instance;
-    private static final String keyAlias = "AESPasswordsEncryptionKey";
 
     private Cipher cipher;
     private SecretKey key;
+    private IvParameterSpec iv;
 
     private KeyStore keyStore;
     private char[] passwordC;
 
-    private CryptoHandler(Context context) {
+    protected void generate(boolean generateKey, boolean generateIv, Context context)
+            throws CertificateException,
+            IOException,
+            NoSuchAlgorithmException,
+            KeyStoreException {
+        if (!(generateKey || generateIv)) {
+            Log.i(TAG, "Didn't generate key or iv, this is a problem if there is another file not" +
+                    " found exception above that mentions these secrets");
+            return;
+        }
+
+        KeyStore.ProtectionParameter protectionParameter = new KeyStore.PasswordProtection(passwordC);
+        keyStore.load(null, passwordC);
+        FileOutputStream fos = new FileOutputStream(new File(context.getFilesDir(), keyStoreFileName));
+
+        if (generateKey) {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+            keyGenerator.init(256);
+            key = keyGenerator.generateKey();
+            KeyStore.SecretKeyEntry secretKeyEntry = new KeyStore.SecretKeyEntry(key);
+            keyStore.setEntry(keyAlias, secretKeyEntry, protectionParameter);
+            keyStore.store(fos, passwordC);
+        }
+
+        if (generateIv) {
+            byte[] ivBytes = new byte[16];
+            new SecureRandom().nextBytes(ivBytes);
+            iv = new IvParameterSpec(ivBytes);
+            KeyStore.SecretKeyEntry secretKeyEntry = new KeyStore.SecretKeyEntry(new SecretKeySpec(ivBytes, "AES"));
+            keyStore.setEntry(ivAlias, secretKeyEntry, protectionParameter);
+            keyStore.store(fos, passwordC);
+        }
+
+    }
+
+    protected CryptoHandler(Context context) {
         String passwordS = "_somepassword_";
         passwordC = new char[passwordS.length()];
         passwordS.getChars(0, passwordS.length(), passwordC, 0);
 
+        boolean generateKey = false;
+        boolean generateIv = false;
+
         try { //try to read the existing key
-            //TODO: replace algorithm with AES/
             cipher = Cipher.getInstance("AES/CBC/NoPadding");
             keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
 
-            File keyStorageFile = StorageController.getInstance().findFile(context.getFilesDir(), "keystorage");
-            if (keyStorageFile == null)
-                throw new FileNotFoundException("keystorage file was not found");
+            File keyStorageFile = StorageController.getInstance().findFile(context.getFilesDir(), keyStoreFileName);
+            if (keyStorageFile == null) {
+                generateIv = generateKey = true;
+                throw new FileNotFoundException(keyStoreFileName + " file was not found");
+            }
             FileInputStream fileInputStream = new FileInputStream(keyStorageFile);
             keyStore.load(fileInputStream, passwordC);
 
             KeyStore.ProtectionParameter protectionParameter = new KeyStore.PasswordProtection(passwordC);
 
             KeyStore.SecretKeyEntry secretKeyEntry = (KeyStore.SecretKeyEntry) keyStore.getEntry(keyAlias, protectionParameter);
-            key = secretKeyEntry.getSecretKey();
-        } catch (FileNotFoundException e) { //key doesn't exist, generate a new key and store it
-            Log.d(TAG, e.getMessage() + "; Resolving by generating a new key...", e);
-            try {
-                KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-                keyGenerator.init(256);
-                key = keyGenerator.generateKey();
+            if (!(generateKey = (secretKeyEntry == null))) key = secretKeyEntry.getSecretKey();
 
-                KeyStore.SecretKeyEntry secretKeyEntry = new KeyStore.SecretKeyEntry(key);
-                KeyStore.ProtectionParameter protectionParameter = new KeyStore.PasswordProtection(passwordC);
-                keyStore.load(null, passwordC);
+            KeyStore.SecretKeyEntry secretIvEntry = (KeyStore.SecretKeyEntry) keyStore.getEntry(ivAlias, protectionParameter);
+            if (!(generateIv = (secretIvEntry == null)))
+                iv = new IvParameterSpec(secretIvEntry.getSecretKey().getEncoded());
+        } catch (FileNotFoundException e) {
+            Log.d(TAG, e.getMessage() + "; Will attempt to resolve by generating a new file containing a key and iv...", e);
+        } catch (Exception e) {
+            Log.d(TAG, e.getMessage(), e);
+        }
 
-                keyStore.setEntry(keyAlias, secretKeyEntry, protectionParameter);
-                FileOutputStream fos = new FileOutputStream(new File(context.getFilesDir(), "keystorage"));
-                keyStore.store(fos, passwordC);
-
-                //cipher.init(Cipher.ENCRYPT_MODE, key);
-            } catch (Exception exception) {
-                Log.d(TAG, exception.getMessage(), exception);
-            }
+        try {
+            generate(generateKey, generateIv, context);
         } catch (Exception e) {
             Log.d(TAG, e.getMessage(), e);
         }
